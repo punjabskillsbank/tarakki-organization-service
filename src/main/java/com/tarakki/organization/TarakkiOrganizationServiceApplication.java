a766f230073986606fe3850424fdcc808cd8709d
Ivkaran Singh <ivkaransingh@macbook-pro.mynetworksettings.com>
2026-03-31 10:25:03 -0400

    project setup

---
+++ src/main/java/com/tarakki/organization/TarakkiOrganizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tarakki.organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarakkiOrganizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarakkiOrganizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarakki/organization/TarakkiOrganizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarakki.organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarakkiOrganizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
