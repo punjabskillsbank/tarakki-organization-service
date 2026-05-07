eae313e65abbc2f0b5bb94f6f0716637917c61b6
Gurwinder-dotcom <gurwinderofficial9@gmail.com>
2026-05-07 15:10:49 +0530

    TK_11: removed file

--- src/main/java/com/tarakki/organization/exceptionhandling/NullOrganizationsOfferedException.java
+++
@@ -1,7 +0,0 @@
-package com.tarakki.organization.exceptionhandling;
-
-public class NullOrganizationsOfferedException extends RuntimeException {
-    public NullOrganizationsOfferedException() {
-        super("Organizations cannot be null.");
-    }
-}
--- src/main/java/com/tarakki/organization/exceptionhandling/OwnerIdNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.tarakki.organization.exceptionhandling;
-
-import java.util.UUID;
-
-public class OwnerIdNotFoundException extends RuntimeException {
-    public OwnerIdNotFoundException(UUID ownerId) {
-        super("Owner not found at given ownerId: " + ownerId);
-    }
-}
