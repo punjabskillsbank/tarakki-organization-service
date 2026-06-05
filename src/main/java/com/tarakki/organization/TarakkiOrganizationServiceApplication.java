df42811ca358f9ac14af48af04eedd976639e72a
Gurwinder-dotcom <gurwinderofficial9@gmail.com>
2026-06-05 17:57:46 +0530

    TK_60: Implement CORS Config

--- src/main/java/com/tarakki/organization/TarakkiOrganizationServiceApplication.java
+++ src/main/java/com/tarakki/organization/TarakkiOrganizationServiceApplication.java
@@ -1,11 +1,14 @@
 package com.tarakki.organization;
 
+import com.tarakki.common.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.tarakki.common.entity")
+@Import(CorsConfig.class)
 public class TarakkiOrganizationServiceApplication {
 
     public static void main(String[] args) {
