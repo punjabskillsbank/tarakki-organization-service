be9afee6ffc4914927e4bdbd3026af962e338c5b
Dhruvjot-codes <dhruvjotsingh21@gmail.com>
2026-06-20 15:29:56 +0530

    TK_40 clean implementation

--- src/main/java/com/tarakki/organization/serviceimpl/OrganizationServiceImpl.java
+++ src/main/java/com/tarakki/organization/serviceimpl/OrganizationServiceImpl.java
@@ -1,8 +1,7 @@
 package com.tarakki.organization.serviceimpl;
 
-import com.tarakki.common.entity.Member;
-import com.tarakki.organization.dto.OrganizationDTO;
 import com.tarakki.common.entity.Organization;
+import com.tarakki.organization.dto.OrganizationDTO;
 import com.tarakki.organization.exceptionhandling.OwnerIdNotFoundException;
 import com.tarakki.organization.repository.MemberRepository;
 import com.tarakki.organization.repository.OrganizationRepository;
@@ -11,7 +10,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -19,12 +18,15 @@ public class OrganizationServiceImpl implements OrganizationService {
     private final OrganizationRepository organizationRepository;
     private final MemberRepository memberRepository;
     private final ModelMapper mapper;
+   
 
+   
     @Override
     @Transactional
     public OrganizationDTO createOrganization(OrganizationDTO organizationRequest) {
-        Optional<Member> owner = Optional.of(memberRepository.findById(organizationRequest.getOwnerId())
-                .orElseThrow(() -> new OwnerIdNotFoundException(organizationRequest.getOwnerId())));
+        memberRepository.findById(organizationRequest.getOwnerId())
+                .orElseThrow(() -> new OwnerIdNotFoundException(organizationRequest.getOwnerId()));
+
         Organization organization = mapper.map(organizationRequest, Organization.class);
         Organization savedOrganization = organizationRepository.save(organization);
         return mapper.map(savedOrganization, OrganizationDTO.class);
