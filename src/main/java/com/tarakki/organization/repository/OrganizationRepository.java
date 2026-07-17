1121ec40f90fe3d02e211282d7f7843534ef3611
codebyjsa <jsaishere@gmail.com>
2026-07-18 01:12:49 +0530

    TK_111: fix existsMemberInOrganization to include organization owner

--- src/main/java/com/tarakki/organization/repository/OrganizationRepository.java
+++ src/main/java/com/tarakki/organization/repository/OrganizationRepository.java
@@ -43,10 +43,11 @@ public interface OrganizationRepository extends JpaRepository<Organization, Long
     List<Map<String, Object>> findAllOrganizationsWithOwnerAndMemberCount();
 
     @Query(value = """
-            SELECT COUNT(*) > 0
-            FROM org_members
-            WHERE org_id = :orgId
-              AND member_id = :memberId
+            SELECT EXISTS (
+                SELECT 1 FROM organizations WHERE org_id = :orgId AND owner_id = :memberId
+                UNION ALL
+                SELECT 1 FROM org_members WHERE org_id = :orgId AND member_id = :memberId
+            )
             """, nativeQuery = true)
     boolean existsMemberInOrganization(@Param("orgId") Long orgId, @Param("memberId") UUID memberId);
 }
