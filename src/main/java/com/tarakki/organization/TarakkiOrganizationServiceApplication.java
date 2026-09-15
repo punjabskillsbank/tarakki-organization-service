87a89dbe55e9f37a51caaae59ec73f0e12ea7e4f
ishmeet-codes <ishmeetsinghxa@gmail.com>
2026-09-15 22:57:59 +0530

    TK_141: JPA Config error

--- src/main/java/com/tarakki/organization/TarakkiOrganizationServiceApplication.java
+++ src/main/java/com/tarakki/organization/TarakkiOrganizationServiceApplication.java
@@ -3,13 +3,9 @@ package com.tarakki.organization;
 import com.tarakki.common.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.tarakki.organization", "com.tarakki.common"})
-@EntityScan(basePackages = {"com.tarakki.common.entity", "com.tarakki.organization.entity"})
-@EnableJpaRepositories(basePackages = {"com.tarakki.organization.repository"})
 @Import(CorsConfig.class)
 public class TarakkiOrganizationServiceApplication {
 
